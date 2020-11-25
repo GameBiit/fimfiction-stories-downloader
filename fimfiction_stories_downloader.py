f24ff4b94a66b3e34d7456bd66b3e7c203d82a04
karwler <karwler@interia.pl>
2020-11-25 16:50:37 +0100

    Command line arguments
    * add command line arguments for setting options and the output folder
    * update README

--- fimfiction_stories_downloader.py
+++ fimfiction_stories_downloader.py
@@ -1,6 +1,8 @@
+import argparse
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
 import os.path
 import sys
 import urllib.parse as urlparse
@@ -8,15 +10,41 @@ import urllib.parse as urlparse
 
 def main_program():
 
+    format_choices = {
+        '/txt': ['1', 'txt', '1-txt'],
+        '/html': ['2', 'html', '2-html'],
+        '/epub': ['3', 'epub', '3-epub']
+    }
+
     class FfsdError(Exception):
         pass
+
+    def parse_command_line_arguments():
+        default_out_dir = 'downloaded_stories'
+
+        cla_parser = argparse.ArgumentParser()
+        cla_parser.add_argument('-o', '--out', default=default_out_dir,
+                                help='set the directory to which all stories will be downloaded '
+                                     f"(default is {default_out_dir})")
+        cla_parser.add_argument('-a', '--adult', help='set whether to download mature rated stories (y/n)')
+        cla_parser.add_argument('-f', '--format', choices=sum(format_choices.values(), []),
+                                help='set the file format for all downloaded stories')
+        cla_parser.add_argument('-r', '--range', choices=['1', '2'],
+                                help='set whether to download only from the current page (1) '
+                                     'or from all pages starting with the current one (2)')
+        cla_parser.usage = __file__ + ' [options] [URLs]'
+        cla_parser.epilog = "If any numbers of URLs are passed, the program will process them and quit. Otherwise " \
+                            "it'll ask the user to input them until an interrupt signal or EOF is encountered." \
+                            "If the rating, format or range isn't passed, the user will be asked to specify the " \
+                            "missing settings for each URL."
+        return cla_parser.parse_known_args()
 
     def create_download_folder():
         """
         Create a download folder if it does not exist
         """
         try:
-            os.mkdir('downloaded_stories')
+            os.mkdir(cla_args[0].out)
         except FileExistsError:
             pass
 
@@ -24,8 +52,13 @@ def main_program():
         """
         Get the url from a user and store the url for further use
         """
-        url = input(
-            "Enter the address of a public or unlisted bookshelf or folder.\nIt has to start with 'https://www': ")
+        if list_link_id == -1:
+            url = input(
+                "Enter the address of a public or unlisted bookshelf or folder.\nIt has to start with 'https://www': ")
+        else:
+            url = cla_args[1][list_link_id]
+            print('Downloading from:', url)
+
         if 'fimfiction.net/story' in url:
             raise FfsdError(
                 "This program cannot download single stories. You need the website address with a list of stories.")
@@ -43,9 +76,14 @@ def main_program():
         """
         new_session = requests.Session()
         jar = requests.cookies.RequestsCookieJar()
-        mature_content = input("\nDo you want to include adult stories?(y/n): ").lower()
-        if mature_content == 'y' or mature_content == 'yes':
+
+        if cla_args[0].adult:
+            mature_content = cla_args[0].adult
+        else:
+            mature_content = input("\nDo you want to include adult stories? (y/n): ")
+        if mature_content.lower() in ['y', 'yes', 'on', '1']:
             jar.set('view_mature', 'true')
+
         jar.set('d_browse_bookshelf', '2')  # grid-like view
         new_session.cookies = jar
         return new_session
@@ -65,15 +103,15 @@ def main_program():
         """
         Choose the format of story
         """
+        if cla_args[0].format:
+            return next(key for key, values in format_choices.items() if cla_args[0].format in values)
+
         while True:
-            chosen_file_format = input('\nChoose the file format (enter a number): 1-txt, 2-html, 3-epub: ').lower()
-            if chosen_file_format == '1' or chosen_file_format == 'txt' or chosen_file_format == '1-txt':
-                return '/txt'
-            if chosen_file_format == '2' or chosen_file_format == 'html' or chosen_file_format == '2-html':
-                return '/html'
-            if chosen_file_format == '3' or chosen_file_format == 'epub' or chosen_file_format == '3-epub':
-                return '/epub'
-            print("You entered something incorrect. Try again.")
+            try:
+                chosen_file_format = input('\nChoose the file format (enter a number): 1-txt, 2-html, 3-epub: ').lower()
+                return next(key for key, values in format_choices.items() if chosen_file_format in values)
+            except StopIteration:
+                print("You entered something incorrect. Try again.")
 
     def range_of_pages(soup):
         """
@@ -91,16 +129,20 @@ def main_program():
         else:
             list_of_pages = soup.find(class_='page_list')  # more than one page and not the last page
             end_page = int(list_of_pages.findAll('a', href=True)[-2].text)
-            while end_page != current_page:
-                users_range_of_pages = input("\nWhat do you want to download? (enter '1' or '2'):\n"
-                                             "1-only stories from the current page\n"
-                                             "2-stories from all pages starting from the current one\n")
-                if users_range_of_pages == "1":
-                    end_page = current_page
-                elif users_range_of_pages == "2":
-                    break
-                else:
-                    print("You entered something incorrect. Try again!")
+
+            if cla_args[0].range == '1':
+                end_page = current_page
+            elif not cla_args[0].range:
+                while end_page != current_page:
+                    users_range_of_pages = input("\nWhat do you want to download? (enter '1' or '2'):\n"
+                                                 "1-only stories from the current page\n"
+                                                 "2-stories from all pages starting from the current one\n")
+                    if users_range_of_pages == "1":
+                        end_page = current_page
+                    elif users_range_of_pages == "2":
+                        break
+                    else:
+                        print("You entered something incorrect. Try again!")
         return current_page, end_page
 
     def stories_and_pages_loop():
@@ -168,21 +210,23 @@ def main_program():
                 filename = story_id + '.' + story_link.split('/')[-1]  # fallback to 'id.ext'
 
             stripped_filename = filename.translate(str.maketrans(translator))
-            download_path = check_filepath(os.path.join('downloaded_stories', stripped_filename), story_id)
+            download_path = check_filepath(os.path.join(cla_args[0].out, stripped_filename), story_id)
             with open(download_path, 'wb') as file:
                 file.write(fetched_file.content)
-        print("Your stories have been downloaded.",
-              "Check the folder 'downloaded_stories' in the folder with this program.\n")
+        print(f'Your stories have been downloaded to "{os.path.join(os.getcwd(), cla_args[0].out)}".')
 
-    while True:
+    cla_args = parse_command_line_arguments()
+    list_link_id = -1 if len(cla_args[1]) == 0 else 0
+    while list_link_id < len(cla_args[1]):
         try:
             website_url, parsed_url, url_query = get_the_website_address()
             session = establish_a_session()
-            create_download_folder()
             output = choose_file_format()
+            create_download_folder()
             save_files()
         except FfsdError as err:
             print(err)
+        list_link_id += 1 if list_link_id != -1 else 0
 
 
 if __name__ == "__main__":
